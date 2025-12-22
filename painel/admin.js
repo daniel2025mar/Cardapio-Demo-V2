8f99e086794a73f3acdf36d110f5696f494bb276
Daniel Dimas <djdaniel095@gmail.com>
2025-12-22 11:50:17 -0300

    Corrigido caminho do atendimentos.html e modal WhatsApp

--- painel/admin.js
+++ painel/admin.js
@@ -2109,7 +2109,7 @@ async function salvarHorariosSemana() {
     }
   }
 
-  alert("Configurações salvas com sucesso!");
+  
 }
 
 // ===============================
@@ -2976,7 +2976,7 @@ document.addEventListener("DOMContentLoaded", () => {
       fecharModalSenha();
 
       // 🔥 CAMINHO CORRETO
-      window.location.href = "painel-whatsapp/atendimentos.html";
+      window.location.href = "../painel-whatsapp/atendimentos.html";
 
     } catch (err) {
       console.error("Erro ao validar senha:", err);
